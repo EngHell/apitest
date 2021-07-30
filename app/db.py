f529a0079301f61d5f641be6a951293576a4f20e
Angel Recinos <migueln6@gmail.com>
2021-07-30 11:43:32 -0600

    splitted the code in more files.

---
+++ app/db.py
@@ -0,0 +1,16 @@
+from models import UserInDB
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
